5a651e3718c0e28f5cac89bfce08b85c5c853d2c
Himanshu Ganapvarapu <himanshu.ganpa@gmail.com>
2025-02-28 23:56:42 +0530

    Upgrading React Versioni

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import { Home, AboutUs, Products, ContactUs, Gallery } from '../components';
 import ROUTES from '../constants/route-consts';
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path={'/'} component={Home} />
-      <Route exact path={ROUTES.HOME.path} component={Home} />
-      <Route exact path={ROUTES.ABOUT_US.path} component={AboutUs} />
-      <Route exact path={ROUTES.PRODUCTS.path} component={Products} />
-      <Route exact path={ROUTES.CONTACT_US.path} component={ContactUs} />
-      <Route exact path={ROUTES.GALLERY.path} component={Gallery} />
-    </Switch>
+    <RouterRoutes>
+      <Route path={'/'} element={<Home />} />
+      <Route path={ROUTES.HOME.path} element={<Home />} />
+      <Route path={ROUTES.ABOUT_US.path} element={<AboutUs />} />
+      <Route path={ROUTES.PRODUCTS.path} element={<Products />} />
+      <Route path={ROUTES.CONTACT_US.path} element={<ContactUs />} />
+      <Route path={ROUTES.GALLERY.path} element={<Gallery />} />
+    </RouterRoutes>
   );
 };
 
